a5ad47c0067ab5d8e838d68dd408d72ccb01d3a1
Szabó Bence <szabobnc12@gmail.com>
2025-10-09 21:37:50 +0200

    make newTransaction modal and input fields required

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,18 +1,27 @@
 import { Link, Outlet, useNavigate } from "react-router-dom";
 import { useAuth } from './context/AuthContext';
+import { useState } from "react";
+import NewTransaction from "./NewTransaction";
 
 function Layout() {
     const { logout } = useAuth();
+    const [showTransaction, setShowTransaction] = useState(false)
 
-    return(
-        <div className="navbar">
-            <nav><Link to="/newTransaction">Add new transaction</Link></nav>
-            <nav><Link to="/main">Add new saving goal</Link></nav>
-            <nav><Link to="/main">add new category</Link></nav>
-            <button onClick={() => logout()}>Logout</button>
-            <Outlet />
-        </div>
+    return (
+        <>
+            <div className="navbar">
+                <nav onClick={() => setShowTransaction(true)}>Add new transaction</nav>
+                <nav><Link to="/main">Add new saving goal</Link></nav>
+                <nav><Link to="/main">add new category</Link></nav>
+                <button onClick={() => logout()}>Logout</button>
+                <Outlet />
+            </div >
+            {showTransaction && (
+                <NewTransaction onClose={() => setShowTransaction(false)} />
+            )
+            }
+        </>
     );
-}   
+}
 
 export default Layout;
--- frontend/src/components/NewTransaction.jsx
+++ frontend/src/components/NewTransaction.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 
 const apiUrl = process.env.REACT_APP_API_BASE_URL;
 
-function NewTransaction() {
+function NewTransaction({ onClose }) {
 
     const { user, loading } = useAuth();
 
@@ -37,7 +37,7 @@ function NewTransaction() {
     }, [loading]);
 
     const initDetails = () => {
-        if(!loading && user){
+        if (!loading && user) {
             setDetails({
                 user: user.id,
                 is_income: '',
@@ -53,9 +53,10 @@ function NewTransaction() {
         e.preventDefault();
         try {
             const response = await axios.post(`${apiUrl}api/createTransaction/`, details);
+            onClose();
         } catch (error) {
-
-        }
+            console.error(error);
+        }   
 
     }
 
@@ -65,33 +66,40 @@ function NewTransaction() {
         )
     } else {
         return (
-            <div>
-                <fieldset>
-                    <form onSubmit={handleSubmit}>
-                        <h1>Create transaction</h1>
-                        <input type="radio" id="income" name="is_income" value="true" checked={details.is_income === 'true'} onChange={(e) => setDetails({ ...details, is_income: e.target.value })} />
-                        <label htmlFor="income">Income</label>
-                        <input type="radio" id="expenditure" name="is_income" value="false" checked={details.is_income === 'false'} onChange={(e) => setDetails({ ...details, is_income: e.target.value })} />
-                        <label htmlFor="expenditure">Expenditure</label>
+            <div className="overlay">
+                <div className="modal">
+                    <fieldset>
+                        <form onSubmit={handleSubmit}>
+                            <h1>Create transaction</h1>
+                            <label htmlFor="income">
+                                <input type="radio" id="income" name="is_income" value="true" checked={details.is_income === 'true'} onChange={(e) => setDetails({ ...details, is_income: e.target.value })} required/>
+                                Income
+                            </label>
+                            <label htmlFor="expenditure">
+                                <input type="radio" id="expenditure" name="is_income" value="false" checked={details.is_income === 'false'} onChange={(e) => setDetails({ ...details, is_income: e.target.value })} required/>
+                                Expenditure
+                            </label>
 
-                        <label htmlFor="category">Category</label>
-                        <select name="category" id="category" onChange={(e) => setDetails({ ...details, category: e.target.value })}>
-                            <option value="" selected>Choose a category </option>
-                            {categories.map((c, i) => (
-                                <option value={c.id}>{c.name}</option>
-                            ))}
-                        </select>
+                            <label htmlFor="category">Category</label>
+                            <select name="category" id="category" onChange={(e) => setDetails({ ...details, category: e.target.value })} required>
+                                <option value="" selected>Choose a category </option>
+                                {categories.map((c, i) => (
+                                    <option value={c.id}>{c.name}</option>
+                                ))}
+                            </select>
 
-                        <label htmlFor="amount">Amount</label>
-                        <input type="number" name="amount" id="amount" onChange={(e) => setDetails({ ...details, amount: e.target.value })} />
+                            <label htmlFor="amount">Amount</label>
+                            <input type="number" name="amount" id="amount" onChange={(e) => setDetails({ ...details, amount: e.target.value })} required/>
 
-                        <label htmlFor="description">Description</label>
-                        <input type="text" name="description" id="description" onChange={(e) => setDetails({ ...details, description: e.target.value })} />
+                            <label htmlFor="description">Description</label>
+                            <input type="text" name="description" id="description" onChange={(e) => setDetails({ ...details, description: e.target.value })} required/>
 
-                        <button type="submit">Save</button>
-                        <button type="reset" onClick={initDetails}>Cancel</button>
-                    </form>
-                </fieldset>
+                            <button type="submit">Save</button>
+                            <button type="reset" onClick={initDetails}>Reset</button>
+                            <button onClick={onClose}>Close</button>
+                        </form>
+                    </fieldset>
+                </div>
             </div>
         );
     }
