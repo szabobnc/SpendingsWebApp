ca4d282cc1442e755a3d9fcda2022716fef12f79
Szabó Bence <szabobnc12@gmail.com>
2025-11-29 17:38:42 +0100

    refactor css

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style/index.css';
-import './style/theme.css';
+//import './style/theme.css';
 import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
